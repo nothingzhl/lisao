8862a041a27876199ea2cad6bb8fdd5b6e861c1d
zhanghanlin <zhaghanlinhs@163.com>
2020-12-01 11:40:47 +0800

    change gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,14 @@ allprojects {
     group = "org.zhl"
     version = "1.0"
 
-    ext{
-        set("guavaVersion","29.0-jre")
-        set("junitVersion","5.6.2")
-        set("lombokVersion","1.18.16")
+    ext {
+        set("guavaVersion", "29.0-jre")
+        set("junitVersion", "5.6.2")
+        set("lombokVersion", "1.18.16")
+        set("log4jVersion", "2.14.0")
     }
+
+
 }
 
 subprojects {
@@ -22,22 +25,23 @@ subprojects {
 
     dependencies {
 
-        val guavaVersion = rootProject.ext["guavaVersion"]
-        val junitVersion = rootProject.ext["junitVersion"]
-        val lombokVersion = rootProject.ext["lombokVersion"]
+        var guavaVersion = rootProject.ext["guavaVersion"] as String?
+        val junitVersion = rootProject.ext["junitVersion"] as String?
+        val lombokVersion = rootProject.ext["lombokVersion"] as String?
 
-        "implementation"("com.google.guava:guava:${guavaVersion}")
+        "implementation"("com.google.guava", "guava", "$guavaVersion")
 
-        "testImplementation"("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
-        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
-        "testImplementation"("org.junit.jupiter:junit-jupiter-params:${junitVersion}")
 
-        "compileOnly"("org.projectlombok:lombok:${lombokVersion}")
-        "annotationProcessor"("org.projectlombok:lombok:1.18.16")
-        "testCompileOnly"("org.projectlombok:lombok:${lombokVersion}")
-        "testAnnotationProcessor"("org.projectlombok:lombok:1.18.16")
+        "testImplementation"("org.junit.jupiter", "junit-jupiter-api", "$junitVersion")
+        "testRuntimeOnly"("org.junit.jupiter", "junit-jupiter-engine", "$junitVersion")
+        "testImplementation"("org.junit.jupiter", "junit-jupiter-params", "$junitVersion")
+
+        "compileOnly"("org.projectlombok", "lombok", "$lombokVersion")
+        "annotationProcessor"("org.projectlombok", "lombok", "$lombokVersion")
+        "testCompileOnly"("org.projectlombok", "lombok", "$lombokVersion")
+        "testAnnotationProcessor"("org.projectlombok", "lombok", "$lombokVersion")
     }
-    
+
     val test by tasks.getting(Test::class) {
         useJUnitPlatform()
     }
@@ -46,6 +50,7 @@ subprojects {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
 
 
--- junit5/build.gradle.kts
+++ junit5/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
-    implementation(group = "org.apache.logging.log4j", name = "log4j-api", version = "2.14.0")
-    implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.14.0")
+
+    var log4jVersion = rootProject.ext["log4jVersion"] as String?
+
+    implementation("org.apache.logging.log4j", "log4j-api", "$log4jVersion")
+    implementation("org.apache.logging.log4j", "log4j-core", "$log4jVersion")
 }
